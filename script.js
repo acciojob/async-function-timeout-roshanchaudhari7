adf2ec212ec3c3e9ddb917e8422bea39d0d763f7
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-09-24 20:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const submitBtn = document.getElementById('btn');
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const outputDisplay = document.getElementById('output');
+submitBtn.addEventListener('click', async function() {
+    // Get the user input values
+    let textValue = textInput.value;
+    let delayTimeValue = delayInput.value;
+    outputDisplay.innerHTML = '';
+
+    // Wait for the specified delay before resolving the promise.
+    await delay(delayTimeValue);
+
+    // Display the text after the delay
+    outputDisplay.innerText = textValue;
+});
